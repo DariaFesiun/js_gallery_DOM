75f02a2b4e35c026995c0dd919d0a3fa5b669ade
daradanilova <lemma1102@gmail.com>
2024-08-08 13:28:25 +0300

    make build

---
+++ index.6fd53745.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){if(t.preventDefault(),"IMG"===t.target.tagName){var n=t.target.parentElement.href;e.src=n}});
+//# sourceMappingURL=index.6fd53745.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+const thumbs = document.getElementById("thumbs");
+thumbs.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (e.target.tagName === "IMG") {
+        const newSrc = e.target.parentElement.href;
+        largeImg.src = newSrc;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
